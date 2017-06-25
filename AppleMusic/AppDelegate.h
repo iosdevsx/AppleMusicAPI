2351083075728e39b647360fc374d915017dc855
Yury Loginov <yloginov@swiftcom.uk>
2017-06-25 17:11:07 +0300

    Initial Commit

---
+++ AppleMusic/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AppleMusic
+//
+//  Created by Юрий Логинов on 25.06.17.
+//  Copyright © 2017 Юрий Логинов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
